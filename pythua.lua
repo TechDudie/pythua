8b6e265ab08be078772644eecf8b35b92188d938
TechDudie <73961295+TechDudie@users.noreply.github.com>
2020-11-10 08:25:12 -0600

    Update pythua.lua

--- pythua.lua
+++ pythua.lua
@@ -1,5 +1,12 @@
-function read(filename)
+function readfile(filename)
   file = io.open(filename, "r")
   io.input(file)
   return io.read()
+end
+
+function cleanread()
+  file = io.open(filename, "w+")
+  io.output(file)
+  io.write("")
+  io.close()
 end
